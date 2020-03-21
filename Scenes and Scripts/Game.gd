e0b7acd4bd187221a6e7622217161df61aa68b90
daerSeebaer <61566539+daerSeebaer@users.noreply.github.com>
2020-03-21 23:54:31 +0100

    Main UI: graphs can now fit actual data
    Graphs fit calculated data (for now only those in the COVID19 tab). Also prepared "Aktuelles" box to be scrollable

--- Scenes and Scripts/Game.gd
+++ Scenes and Scripts/Game.gd
@@ -48,13 +48,9 @@ func _on_Geld_draw():
 
 func _on_Button_TagEnde_pressed():
 	# trigger events?
+	
+	# calculate new numbers
 	logic.FinishDay()
-	
-	print(logic.newInfected[-1])
-	
-	# calculate infection counts --> mit Mechanikern koordinieren
-	 # TESTARRAY
-	#var arr = [1, 2, 3]
 	
 	# calculate other values
 	
@@ -67,16 +63,50 @@ func _on_Button_TagEnde_pressed():
 	# update other KPIs
 	
 	# calculate the Graphs
-	# for Covid-Screen: -200;80 -> 750;400
 	
-	# arr wird durch werte z.B. Anzahl infizierte ersetzt
-	#var y_max = arr.max()
-	#var y_min = arr.min()
-		
 	
-	#for y in range(arr.size()):
-	#	print(y)
+	var temp_points = []
+	# for Covid-Screen: -200;80 -> 850;-400
+	temp_points = _normalize_Graph_Points(-200, 850, 80, -400, logic.infectedByDay)
+	$"Control/TabContainer/COVID-19 Zahlen/Line2D_infizierte".clear_points()
+	for point in temp_points:
+		$"Control/TabContainer/COVID-19 Zahlen/Line2D_infizierte".add_point(point)
+	
+	temp_points = _normalize_Graph_Points(-200, 850, 80, -400, logic.deadByDay)
+	$"Control/TabContainer/COVID-19 Zahlen/Line2D_Tote".clear_points()
+	for point in temp_points:
+		$"Control/TabContainer/COVID-19 Zahlen/Line2D_Tote".add_point(point)
+	
+	temp_points = _normalize_Graph_Points(-200, 850, 80, -400, logic.curedByDay)
+	$"Control/TabContainer/COVID-19 Zahlen/Line2D_Genesene".clear_points()
+	for point in temp_points:
+		$"Control/TabContainer/COVID-19 Zahlen/Line2D_Genesene".add_point(point)
 	
 	# enter new entries to the newsfeed
 	
 	# enter new entries to the eventlog
+
+func _normalize_Graph_Points(frame_x_min, frame_x_max, frame_y_min, frame_y_max, input_array):
+	var return_array = []
+	var x_temp_px
+	var y_temp_px
+	var x_step_size
+	# read border values
+	var y_max_value = input_array.max()
+	var y_min_value = input_array.min()
+	
+	var y_diff_px = frame_y_max - frame_y_min
+	var y_diff_value = y_max_value - y_min_value
+	
+	# calculate x step size
+	x_step_size = ( frame_x_max - frame_x_min ) / input_array.size()
+	
+	# normalize y values and write to return
+	for i in input_array.size():
+		x_temp_px = frame_x_min + x_step_size * i
+		if y_diff_value == 0: y_temp_px = 0
+		else:
+			y_temp_px =  ( ( input_array[i] - y_min_value ) / y_diff_value ) * y_diff_px + frame_y_min
+		return_array.push_back(Vector2(x_temp_px, y_temp_px))
+	
+	return return_array
