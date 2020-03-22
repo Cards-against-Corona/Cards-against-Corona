1a094102dafb8afc4e1c2e6648a9a911acc3567b
Harutami <spieleok@gmail.com>
2020-03-22 14:59:47 +0100

    ein # hinugefügt

--- Scenes and Scripts/View/EineKarte.gd
+++ Scenes and Scripts/View/EineKarte.gd
@@ -23,34 +23,34 @@ func _ready():
 	$"Effektivität".set_text(str(cardModel.effect))
 	$Beschreibung.set_text(cardModel.beschreibung)
 	#$Sprite.set_texture(iconpath)
-	if int(UrsacheHealth) > 0:
-		$"Control/Gesundheitswesen_status/up".show()
-	elif int(UrsacheHealth) < 0:
-		$"Control/Gesundheitswesen_status/down".show()
-	else:
-		pass
-	if int(UrsacheBefriedigung) > 0:
-		$"Control/Zufriedenheit_status/up".show()
-	elif int(UrsacheBefriedigung) < 0:
-		$"Control/Zufriedenheit_status/down".show()
-	else:
-		pass
-	if int(Geldkosten) > 0:
-		$"Control/wirtschaft_status/up".show()
-	elif int(Geldkosten) < 0:
-		$"Control/wirtschaft_status/down".show()
-	else:
-		pass
-	if int(Securitykosten) > 0:
-		$"Control/Sicherheitskräfte_status/up".show()
-	elif int(Securitykosten) < 0:
-		$"Control/Sicherheitskräfte_status/down".show()
-	else:
-		pass
-	if int(Bettenkosten) > 0:
-		$"Control/intensivbetten_status/up".show()
-	elif int(Bettenkosten) < 0:
-		$"Control/intensivbetten_status/down".show()
-	else:
-		pass
+#	if int(UrsacheHealth) > 0:
+#		$"Control/Gesundheitswesen_status/up".show()
+#	elif int(UrsacheHealth) < 0:
+#		$"Control/Gesundheitswesen_status/down".show()
+#	else:
+#		pass
+#	if int(UrsacheBefriedigung) > 0:
+#		$"Control/Zufriedenheit_status/up".show()
+#	elif int(UrsacheBefriedigung) < 0:
+#		$"Control/Zufriedenheit_status/down".show()
+#	else:
+#		pass
+#	if int(Geldkosten) > 0:
+#		$"Control/wirtschaft_status/up".show()
+#	elif int(Geldkosten) < 0:
+#		$"Control/wirtschaft_status/down".show()
+#	else:
+#		pass
+#	if int(Securitykosten) > 0:
+#		$"Control/Sicherheitskräfte_status/up".show()
+#	elif int(Securitykosten) < 0:
+#		$"Control/Sicherheitskräfte_status/down".show()
+#	else:
+#		pass
+#	if int(Bettenkosten) > 0:
+#		$"Control/intensivbetten_status/up".show()
+#	elif int(Bettenkosten) < 0:
+#		$"Control/intensivbetten_status/down".show()
+#	else:
+#		pass
 
