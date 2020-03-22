9bf78b3cb1e6d771dd088a0ee2de0935553e9d30
Harutami <spieleok@gmail.com>
2020-03-22 14:40:19 +0100

    Karten nochmal besser gemacht, sowie die Karten- übersicht

--- Scenes and Scripts/View/EineKarte.gd
+++ Scenes and Scripts/View/EineKarte.gd
@@ -19,7 +19,36 @@ func _ready():
 	$"Effektivität".set_text(str(Effekt))
 	$Beschreibung.set_text(Beschreibung)
 	#$Sprite.set_texture(iconpath)
-	
+	if int(UrsacheHealth) > 0:
+		$"Control/Gesundheitswesen_status/up".show()
+	elif int(UrsacheHealth) < 0:
+		$"Control/Gesundheitswesen_status/down".show()
+	else:
+		pass
+	if int(UrsacheBefriedigung) > 0:
+		$"Control/Zufriedenheit_status/up".show()
+	elif int(UrsacheBefriedigung) < 0:
+		$"Control/Zufriedenheit_status/down".show()
+	else:
+		pass
+	if int(Geldkosten) > 0:
+		$"Control/wirtschaft_status/up".show()
+	elif int(Geldkosten) < 0:
+		$"Control/wirtschaft_status/down".show()
+	else:
+		pass
+	if int(Securitykosten) > 0:
+		$"Control/Sicherheitskräfte_status/up".show()
+	elif int(Securitykosten) < 0:
+		$"Control/Sicherheitskräfte_status/down".show()
+	else:
+		pass
+	if int(Bettenkosten) > 0:
+		$"Control/intensivbetten_status/up".show()
+	elif int(Bettenkosten) < 0:
+		$"Control/intensivbetten_status/down".show()
+	else:
+		pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
